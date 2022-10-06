cdfec212604afb97c2a51378aa7176711881fd7a
cc54321 <112970207+cc54321@users.noreply.github.com>
2022-10-06 09:43:06 -0400

    Create big-number.js

---
+++ big-number.js
@@ -0,0 +1,15 @@
+function bigNumber(num1, num2){
+    if (num1 > num2){
+        return num1
+    
+    }else if(num2 > num1){
+        return num2
+
+    }else{
+        return "there equal"
+    }
+    
+}
+console.log(bigNumber(25, 40));
+
+
